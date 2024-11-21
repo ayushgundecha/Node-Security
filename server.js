ab3a081d3fcffc5c5ddaeaf5e8408d8cff6e9010
ayushgundecha <ayushgundecha123@gmail.com>
2024-11-21 23:54:41 +0530

    added passport.js

--- server.js
+++ server.js
@@ -3,13 +3,34 @@ const path = require('path');
 const https = require('https');
 const express = require('express');
 const helmet = require('helmet');
+const passport = require('passport');
+const { Strategy } = require('passport-google-oauth20');
 
+require('dotenv').config();
 
 const PORT = 8000;
+
+const config = {
+    CLIENT_ID : process.env.CLIENT_ID,
+    CLIENT_SECERT: process.env.CLIENT_SECERT
+}
 
 const app = express();
 
 app.use(helmet());
+
+const AUTH_OPTIONS = {
+    callbackURL : '/auth/google/callback',
+    clientID : config.CLIENT_ID,
+    clientSecret : config.CLIENT_SECERT
+}
+
+function verifyCallBack(accessToken, refreshToken, profile, done){
+    console.log('Google profile', profile);
+    done(null, profile);
+}
+
+passport.use(new Strategy(AUTH_OPTIONS, verifyCallBack));
 
 function checkLoggedIn(req, res, next){
     const isLoggedIn = true; //todo
